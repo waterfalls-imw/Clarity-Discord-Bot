b9df854b73e5305752161edce9c365d03519a3dc
Imogen Waters <imogenwf@gmail.com>
2021-01-08 18:44:13 +0800

    Changed the relevant channels to be pulled by id as well as by name

--- main.js
+++ main.js
@@ -124,14 +124,14 @@ client.once('ready', () => {
 
 client.on('message', async message => {
     if (message.author.bot) return;
-    if (message.channel.name === 'proposal-voting'){
+    if (message.channel.name === 'proposal-voting' || message.channel.id === '787342513228873748'){
         let messageEmojiArray = message.content.match(/<:.+?:\d+>|(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g);
             // Gather all custom Discord emotes and unicode emojis from the message into the messageEmojiArray 
             // ("/<:.+?:\d+>" matches the custom emotes, the rest matches all unicode emojis)
         if (messageEmojiArray !== null) {
             addReactions(message, messageEmojiArray);  
         }
-    } else if (message.channel.name === 'vouching-room-test') {
+    } else if (message.channel.name === 'vouching-room' || message.channel.id === '787343712442777610') {
         const vouchEmojiArray = ['🟢','🟡','🔴','⚪'];
         addReactions(message, vouchEmojiArray);
     }
