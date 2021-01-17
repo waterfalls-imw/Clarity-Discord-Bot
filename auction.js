4e43beafad440aeb5c0c7824e169487a836c70f9
Imogen Waters <imogenwf@gmail.com>
2021-01-17 16:36:01 +0000

    HELP

--- auction.js
+++ auction.js
@@ -1,21 +1,11 @@
-const Discord = require('discord.js');
-const Sequelize = require('sequelize');
-const captain = require('./captain.js');
+const { Team } = require('discord.js');
+var captain = require('./captain.js');
 
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: 'auction.sqlite',
-});
-
-const Auction = sequelize.define('auction', {
-    round: {
-        type: Sequelize.INTEGER,
-        unique: true,
-    },
-    bidCoins: Sequelize.INTEGER,
-    playerBid: Sequelize.STRING,
-    captainBidding: Sequelize.STRING,
-});
+module.exports = class Auction {
+    captains = [];
+    round = 1;
+    constructor (captains, round) {
+        this.captains = captains;
+        this.round = round;
+    }
+}
